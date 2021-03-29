a27652e217f70969fd26c6024a519f2951c3d59b
zakiladj <ladj.zah@gmail.com>
2021-03-29 07:46:41 +0100

    add the url in our application

---
+++ post/url.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('',views.post_list,name="post_list"),
+    path('post/<int:id>',views.post_detail,name="post_detail"),
+]
+app_name = 'post'
--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -20,7 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('post.url' , namespace = 'blog')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
